7d74b4ce46d7f01e9e58fa3cbe617b913fb607c6
danihdez96 <47672374+danihdez96@users.noreply.github.com>
2019-05-02 18:53:36 -0600

    Add files via upload

---
+++ BaseDatos/CapaPresentacion/frmVendor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using capaNego;
+
+namespace CapaPresentacion
+{
+    public partial class frmVendor : Form
+    {
+        ManttoVendor mntt = new ManttoVendor();
+        public frmVendor()
+        {
+            InitializeComponent();
+        }
+
+        private void frmVendor_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ BaseDatos/capaNego/ManttoVendor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using capadedatos;
+namespace capaNego
+{
+    public class ManttoVendor
+    {
+        bdConex conectarse = new bdConex();
+    }
+}
